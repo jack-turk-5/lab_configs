7a141bbfac37f1363303f555cd12b03f68ae8b40
Jack Turk <jackturk5@outlook.com>
2025-04-19 20:34:55 -0500

    fix alpine entrypoints

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-# Podman/Conmon hands in exactly 2 sockets: FD 3 (UDP dual‑stack) & FD 4 (TCP dual‑stack)
 socat -u FD:3 UDP:[::1]:51820,reuseaddr &
 socat FD:4 TCP-LISTEN:51821,reuseaddr,fork &
-exec "$@"
+exec /usr/bin/dumb-init node server.js
