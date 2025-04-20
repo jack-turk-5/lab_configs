1b5b187c624f4c0e141fbbb6fba26e49934970fd
Jack Turk <jackturk5@outlook.com>
2025-04-20 02:30:51 -0500

    syntax

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -6,7 +6,7 @@ wg-quick up wg0
 
 # 2) Start socket proxies in the background
 socat UDP-LISTEN:51820,reuseaddr,fork FD:3 &
-socat TCP-LISTEN:'[::]':51821,reuseaddr,fork FD:4 &
+socat TCP-LISTEN:[::]:51821,reuseaddr,fork FD:4 &
 
 # 3) Finally exec the UI, inheriting FDs 3 & 4
 exec node server.js
