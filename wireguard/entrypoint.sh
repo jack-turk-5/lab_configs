9769998d25d535453e051ae8edf691279b13b04b
Jack Turk <jackturk5@outlook.com>
2025-04-21 09:43:14 -0500

    wg socat fix

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 set -e
 wg-quick up wg0
-socat - udp:FD:3,sp:58120,fork,reuseaddr &
-socat - tcp-listen:51821,fork,reuseaddr &
-socat - tcp-connect:[::]:51821
+socat -u UDP6-LISTEN:51820,bind=[::],reuseaddr,fork,ipv6only=0 FD:3 &
+socat TCP6-LISTEN:51821,bind=[::],reuseaddr,fork,ipv6only=0 FD:4 &
 exec node server.js
