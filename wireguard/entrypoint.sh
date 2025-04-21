30fa2a337a67d7d943528ea650dc430527969eb8
Jack Turk <jackturk5@outlook.com>
2025-04-21 12:35:28 -0500

    separate sockets

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 set -e
+
+# WireGuard interface up
 wg-quick up wg0
-socat ACCEPT:3,fork TCP:127.0.0.1:51821 &
-exec node server.js
+
+# FD 3: UDP datagram socket (WireGuard handshake)
+# Forward incoming datagrams into wg0 and send outgoing via FD 3
+socat -u FD:3 UDP4-SENDTO:127.0.0.1:51820 &
+socat -u UDP4-LISTEN:51820,reuseaddr,fork FD:3 &
+
+# FD 4: TCP stream socket (UI)
+# Forward incoming connections to the local HTTP server on 127.0.0.1:51821
+socat TCP4-LISTEN:51821,reuseaddr,fork FD:4 &
+
+# Finally, start the UI (which now listens only on loopback)
+exec node /app/server.js
