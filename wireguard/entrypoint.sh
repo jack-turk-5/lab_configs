c27fd4c2b25fdbe744fdbd4781e3f9466d2ba6a0
Jack Turk <jackturk5@outlook.com>
2025-04-21 13:35:49 -0500

    ???

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -e
+# 1) Bring up wg0 (creates the TUN device)
 wg-quick up wg0
-socat -u FD:3 GENERIC:PF_PACKET:SOCK_DGRAM:protocol=htons,interface=wg0 &
+# 2) Proxy UDP from FD 3 into existing wg0
+socat -u FD:3 TUN:,tun-type=tun,tun-name=wg0,iff-up &
+# 3) Proxy TCP from FD 4 to the UI
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
+# 4) Launch Node UI
 exec node /app/server.js
