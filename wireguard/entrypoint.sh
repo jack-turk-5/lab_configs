dccee78f8c4b8c1f2f37baebb3b39743abee2811
Jack Turk <jackturk5@outlook.com>
2025-04-21 11:36:26 -0500

    try other way

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
+wg-quick up wg0
 socat -u UDP6-LISTEN:51820,bind=[::],reuseaddr,fork,ipv6only=0 UDP:127.0.0.1:51820 &
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
-wg-quick up wg0
 exec node server.js
