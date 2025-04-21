0c16062ef76d479b94ce1b48453257e51fcc1d42
Jack Turk <jackturk5@outlook.com>
2025-04-21 14:14:43 -0500

    entrypoint fix for udp

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 # 1) Bring up wg0 (creates the TUN device)
 wg-quick up wg0
-# 2) Proxy UDP from FD 3 into existing wg0
-# socat -u FD:3 TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-up &
 # 3) Proxy TCP from FD 4 to the UI
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
+socat -d -d FD:3,rdonly TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up &
+socat -d -d TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up FD:3,wronly &
 # 4) Launch Node UI
 exec node /app/server.js
