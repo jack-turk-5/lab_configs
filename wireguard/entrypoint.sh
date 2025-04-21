6b8eb651b9bd0454c8ab196fa9f9c31e7c245b56
Jack Turk <jackturk5@outlook.com>
2025-04-21 13:10:50 -0500

    raw packets

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -e
 wg-quick up wg0
-socat -u FD:3 TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-up &
+socat -u FD:3 PACKET:iface=wg0,protocol=ip &
+socat PACKET:iface=wg0,protocol=ip FD:3 &
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
 exec node /app/server.js
