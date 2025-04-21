506bfbc23e12fc157a2c6a82e25bc30d71d21502
Jack Turk <jackturk5@outlook.com>
2025-04-21 13:14:32 -0500

    no way this works

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 set -e
-wg-quick up wg0
-socat -u FD:3 PACKET:0.0.0.0/24,iface=wg0,protocol=ip &
-socat PACKET:0.0.0.0/24,iface=wg0,protocol=ip FD:3 &
+
+# 1) Clone a new TUN device named wg0 and hook FD 3 into it
+socat \
+  -u FD:3 \
+  TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-up &
+
+# 2) Load your WG config onto that tunnel
+wg setconf wg0 /etc/wireguard/wg0.conf
+
+# 3) Proxy TCP UI as before on FD 4:
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
+
+# 4) Start the Node UI
 exec node /app/server.js
