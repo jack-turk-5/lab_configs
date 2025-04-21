9a833f1f636f877a2baaee47bade879da0148452
Jack Turk <jackturk5@outlook.com>
2025-04-21 14:23:17 -0500

    syntax fix

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 set -e
-# 1) Bring up wg0 (creates the TUN device)
 wg-quick up wg0
-# 3) Proxy TCP from FD 4 to the UI
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
-socat -d -d FD:3,rdonly TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up &
-socat -d -d TUN:0.0.0.0/0,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up FD:3,wronly &
-# 4) Launch Node UI
+socat -d -d OPEN:/proc/self/fd/3,rdonly TUN:10.0.10.1/24,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up &
+socat -d -d TUN:10.0.10.1/24,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up OPEN:/proc/self/fd/3,wronly &
 exec node /app/server.js
