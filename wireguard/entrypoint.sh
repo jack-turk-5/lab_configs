b7388d80f9acecd0bfaaeee31f716a7d89f4c5be
Jack Turk <jackturk5@outlook.com>
2025-04-21 10:50:25 -0500

    bidirectional udp

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 set -e
 wg-quick up wg0
-socat -u FD:3 UDP-SENDTO:127.0.0.1:51820 &
-socat -u UDP-RECVFROM:51820,fork,reuseaddr FD:3 &
+socat FD:3 udp:127.0.0.1:51820 &
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
 exec node server.js
