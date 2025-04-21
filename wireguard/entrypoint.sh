5566d742b9d20c4991157f6972f3171054368f88
Jack Turk <jackturk5@outlook.com>
2025-04-21 08:46:13 -0500

    tinker with sockets

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env sh
 set -e
-
-# 1) Bring up WireGuard
 wg-quick up wg0
-
-# 2) Only socket‑activate the UDP path into FD 3:
-#    Listen on UDP 51820, fork, hand datagrams into FD 3
-socat UDP-LISTEN:51820,reuseaddr,fork FD:3 &
-
-# 3) Exec the UI (inherits FD 4 untouched)
+socat - udp:FD:3,sp:58120,fork,reuseaddr &
+socat - tcp-listen:51821,fork,reuseaddr &
+socat - tcp-connect:[::]:51821
 exec node server.js
