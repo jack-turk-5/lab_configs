7bf29e8b2969bb3cddd00a2468a9000d0d001472
Jack Turk <jackturk5@outlook.com>
2025-04-21 12:52:24 -0500

    syntax fix

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
 wg-quick up wg0
-socat -u FD:3 TUN:/dev/net/tun,iff-name=wg0,iff-up &
+socat -u FD:3 TUN:/dev/net/tun,tun-name=wg0,iff-up &
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
 exec node /app/server.js
