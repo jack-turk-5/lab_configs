656e987f5a9e056f4cceb435472eaf4cf35ea40c
Jack Turk <jackturk5@outlook.com>
2025-04-21 13:12:49 -0500

    syntax

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 wg-quick up wg0
-socat -u FD:3 PACKET:iface=wg0,protocol=ip &
-socat PACKET:iface=wg0,protocol=ip FD:3 &
+socat -u FD:3 PACKET:0.0.0.0/24,iface=wg0,protocol=ip &
+socat PACKET:0.0.0.0/24,iface=wg0,protocol=ip FD:3 &
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
 exec node /app/server.js
