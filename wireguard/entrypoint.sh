b6342df5fe74eb9bb7fb80c5011895d5e12cef8e
Jack Turk <jackturk5@outlook.com>
2025-04-21 12:50:06 -0500

    tunnel the connection between socket and wg0 directly

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env sh
 set -e
-
-# WireGuard interface up
 wg-quick up wg0
-
-# Inherited FD 3 (host’s port 51820) → local WireGuard interface
-socat -u FD:3 UDP4-SENDTO:127.0.0.1:51820 &
-
-# New listener on port 51820, using both REUSEADDR and REUSEPORT
-socat -u UDP4-LISTEN:51820,reuseaddr,reuseport,fork FD:3 &
-
-# FD 4: TCP stream socket (UI)
-# Forward incoming connections to the local HTTP server on 127.0.0.1:51821
+socat -u FD:3 TUN:/dev/net/tun,iff-name=wg0,iff-up &
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
-
-# Finally, start the UI (which now listens only on loopback)
 exec node /app/server.js
