6d38109f90cdc25f9746a296c69342b7376964a9
Jack Turk <jackturk5@outlook.com>
2025-04-21 14:25:16 -0500

    wg ip

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 wg-quick up wg0
 socat ACCEPT:4,fork TCP:127.0.0.1:51821 &
-socat -d -d OPEN:/proc/self/fd/3,rdonly TUN:10.0.10.1/24,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up &
-socat -d -d TUN:10.0.10.1/24,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up OPEN:/proc/self/fd/3,wronly &
+socat -d -d OPEN:/proc/self/fd/3,rdonly TUN:10.8.0.1/24,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up &
+socat -d -d TUN:10.8.0.1/24,tun-name=wg0,tun-type=tun,iff-no-pi,iff-up OPEN:/proc/self/fd/3,wronly &
 exec node /app/server.js
