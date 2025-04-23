aed82dd983adb763cf1a1b472d941aee515065dd
Jack Turk <jackturk5@outlook.com>
2025-04-22 20:45:54 -0500

    fix entrypt

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env sh
 set -e
 cd /etc/wireguard
+
+# 1) Generate keys & base config if missing
 if [ ! -f privatekey ]; then
   umask 077
-  wg genkey | tee privatekey \
-    | wg pubkey > publickey
+  wg genkey | tee privatekey | wg pubkey > publickey
   cat > wg0.conf <<EOF
 [Interface]
 PrivateKey = $(cat privatekey)
 ListenPort  = 51820
-Address     = 10.8.0.1/24
 EOF
 fi
+
+# 2) Start BoringTun in foreground on wg0
 boringtun -f wg0 &
-wg setconf wg0 /etc/wireguard/wg0.conf
+
+# 3) Assign IP & bring up the tunnel interface
+ip addr add 10.8.0.1/24 dev wg0
+ip link set wg0 up
+
+# 4) Apply WireGuard config (peers, keys, ports)
+wg setconf wg0 wg0.conf
+
+# 5) Keep the container alive
 wait
